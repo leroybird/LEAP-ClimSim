Fixed calculation of r2 score

arch.py
@@ -1010,16 +1010,15 @@ class Net(nn.Module):
         num_vert=60,
         dim=256,
         depth=20,
-        block=ConvNextBlock2,
-        num_emb=384,
-        emb_ch=32,
-        use_emb: bool = False,
         frac_idxs=None,
-        model_type="transformer",
+        inc_1d_norm = True,
     ):
         super().__init__()
         self.num_2d_in = num_in_2d
         self.num_static = num_static
+        self.inc_1d_norm = inc_1d_norm
+
+        
         num_in_2d = num_in_2d
 
         # if use_emb:
@@ -1049,11 +1048,15 @@ class Net(nn.Module):
                 "b (c z) k -> b (c k) z", c=num_in_2d * mult_fac_2d, z=num_vert, k=1
             ),
         )
-
+        
+        if self.inc_1d_norm:
+            num_3d_in *=  2
+        else:
+            print("No 1D norm")
         self.layer_re_1d = Rearrange("b (c z) -> b c z", z=num_vert)
 
         self.layer_1d_lin = nn.Conv1d(
-            num_3d_in * 2,
+            num_3d_in,
             dim - num_in_2d * mult_fac_2d,
             kernel_size=1,
         )
@@ -1141,7 +1144,10 @@ class Net(nn.Module):
         x_1d = self.layer_re_1d(x_1d)
         
         x_1d_re = rearrange(x_1d_re, "b z c -> b c z")
-        x_1d = self.layer_1d_lin(torch.cat((x_1d, x_1d_re), dim=1))
+        if self.inc_1d_norm:
+            x_1d = torch.cat((x_1d, x_1d_re), dim=1)
+
+        x_1d = self.layer_1d_lin(x_1d)
 
         x_out = self.blocks(torch.cat([xp_1d, x_1d], dim=1))
 

create_submisson.py
@@ -8,7 +8,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # %%
-input_fname = "out_n.pt"
+input_fname = "out_n4.pt"
 offset = 384
 # %%
 preds = torch.load(input_fname)
@@ -76,6 +76,6 @@ out_df.rename(columns={0 : "sample_id"}, inplace=True)
 #%%
 out_df.columns
 # %%
-out_df.to_parquet("norm_small.parquet", index=False)
+out_df.to_parquet("norm_test_200.parquet", index=False)
 #%%
 #%%

norm.py
@@ -162,8 +162,28 @@ def get_stats(loader_cfg: config.LoaderConfig, data_cfg: config.DataConfig):
     else:
         print("Disabling tanh")
         tanh_mults = None
+    
 
-    norm_x = Norm2(stds=stats_x["stds"], means=stats_x["means"], tanh_mults=tanh_mults)
+    if loader_cfg.x_mask_thresh:
+        x_mask = stats_x['x_range'] > loader_cfg.x_mask_thresh
+        assert data_cfg.x_names is not None
+        for n, name in enumerate(data_cfg.x_names):
+            if name.startswith("state_q0001") and int(name.split("_")[-1]) <= 10:
+                x_mask[n] = True
+            if name.startswith("state_q0002") and int(name.split("_")[-1]) <= 15:
+                x_mask[n] = True
+            if name.startswith("state_q0003") and int(name.split("_")[-1]) <= 15:
+                x_mask[n] = True
+
+        print(f"Masking {x_mask.sum()} features")
+        print(f"Masked features: {np.array(data_cfg.x_names)[x_mask]}")
+
+
+    else:
+        x_mask = None
+
+    norm_x = Norm2(stds=stats_x["stds"], means=stats_x["means"], tanh_mults=tanh_mults,
+                   zero_mask=x_mask)
     # norm_x = Norm(fname=loader_cfg.x_stats_path, eps=1e-7)
     # Set means to zero for q vars so we can predict a multiplier
     # norm_x.means[:, data_cfg.fac_idxs[0] : data_cfg.fac_idxs[1]] = 0.0

train.py
@@ -83,7 +83,7 @@ def mse_point(pred, tar):
 
 class LitModel(L.LightningModule):
     def __init__(
-        self, model, cfg_data, cfg_loader, setup_dataloader=True, pt_compile=False, use_robust=False
+        self, model, cfg_data, cfg_loader, setup_dataloader=True, pt_compile=False, use_robust=True
     ):
         super().__init__()
 
@@ -111,8 +111,6 @@ class LitModel(L.LightningModule):
 
         self.val_metrics = [
             fv.mae,
-            fv.mse,
-            r_squared,
             mse_t,
             mse_q1,
             mse_q2,
@@ -136,7 +134,8 @@ class LitModel(L.LightningModule):
         self.use_schedulefree = True
         self.mask = torch.zeros(360 + 8, dtype=torch.bool)
         self.mask[:] = True
-
+        
+        self.residuals = []
         # self.mask[0:60] = True
         # self.mask[240:] = True
 
@@ -153,6 +152,10 @@ class LitModel(L.LightningModule):
         else:
             loss = self.loss_func(pred[:, self.mask], y[:, self.mask])
 
+        if step_name == 'val':
+            self.residuals.append((pred[:, self.mask] - y[:, self.mask]).detach().cpu())
+
+
         if step_name != "train" or batch_idx % 20 == 0:
             self.log(
                 f"{step_name}_loss",
@@ -166,10 +169,11 @@ class LitModel(L.LightningModule):
                 self.log(
                     f"{step_name}_{metric.__name__}",
                     metric(pred, y).item(),
-                    prog_bar=True,
+                    prog_bar=False,
                 )
 
         return loss
+    
 
     def on_train_epoch_start(self):
         if self.use_schedulefree:
@@ -182,6 +186,16 @@ class LitModel(L.LightningModule):
     def on_validation_epoch_end(self):
         if self.use_schedulefree:
             self.opt.train()
+        
+        print('Calculating R2 score...')
+        residuals = torch.cat(self.residuals, dim=0)
+        r2 = 1 - torch.mean(residuals ** 2) / 0.886
+        self.log('val_r2', r2.item(), prog_bar=True, on_epoch=True)
+        mse = torch.mean(residuals ** 2) 
+        self.log('val_mse', mse.item(), prog_bar=True, on_epoch=True)
+
+        self.residuals = []
+
 
     def training_step(self, batch, batch_idx):
         return self.step(batch, self.train_metrics, "train", batch_idx)
@@ -297,22 +311,26 @@ if __name__ == "__main__":
     ]
 
     if not (args.debug or args.lr_find):
-        # Add save model callback
-        checkpoint_callback = ModelCheckpoint(
-            monitor="val_mse",
-            dirpath="checkpoints/",
-            filename="model-{step:06d}-{val_mse:.3f}",
-            save_top_k=3,
-            mode="min",
-        )
-        callbacks.append(checkpoint_callback)
-
+        
         wandb.init(
             project="leap",
             config={**cfg_loader.model_dump(), **cfg_data.model_dump()},
             group="DDP",
         )
         logger = L.pytorch.loggers.WandbLogger()
+        run_name = wandb.run.name if wandb.run is not None else "debug"
+        
+
+        # Add save model callback
+        checkpoint_callback = ModelCheckpoint(
+            monitor="val_mse",
+            dirpath="checkpoints/",
+            filename=f"{run_name}" + "-{step:06d}-{val_mse:.3f}",
+            save_top_k=3,
+            mode="min",
+        )
+        callbacks.append(checkpoint_callback)
+
     else:
         logger = None
 